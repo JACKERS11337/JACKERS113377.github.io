768fc2d69c71c270d461a5750fec64c9fa5916cf
JACKERS113377 <yankovskii2022@mail.ru>
2024-02-04 23:32:53 +0300

    car

--- src/components/Home/Car/VehicleModels/VehicleModels.jsx
+++ src/components/Home/Car/VehicleModels/VehicleModels.jsx
@@ -1,7 +1,6 @@
 import styles from "./VehicleModels.module.css";
 
 const VehicleModels = ({ car }) => {
-  console.log(car.images);
   return (
     <>
       <img className={styles.images} src={car.images} alt="" />
